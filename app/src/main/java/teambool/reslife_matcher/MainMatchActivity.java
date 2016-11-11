263237d82970b7a91902561644e5e83d0dd1c8ed
jwrig5 <jwrig5@unh.newhaven.edu>
2016-11-11 16:30:00 -0500

    bottomsheet added

--- app/src/main/java/teambool/reslife_matcher/MainMatchActivity.java
+++ app/src/main/java/teambool/reslife_matcher/MainMatchActivity.java
@@ -1,16 +1,21 @@
 package teambool.reslife_matcher;
 
 import android.content.BroadcastReceiver;
+import android.support.design.widget.BottomSheetBehavior;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.AsyncTask;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -21,17 +26,42 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
-public class MainMatchActivity extends AppCompatActivity {
+public class MainMatchActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    ///-------------------------- j test
+    // BottomSheetBehavior variable
+    private BottomSheetBehavior bottomSheetBehavior;
+
+    // TextView variable
+    private TextView bottomSheetHeading;
+
+    // Button variables
+    private Button expandBottomSheetButton;
+    private Button collapseBottomSheetButton;
+    private Button hideBottomSheetButton;
+    private Button showBottomSheetDialogButton;
+
+    private TextView mMatchView;
+    ///-------------------------- j test
+
+
     private RetrieveMatches mMatcher;
     private JSONObject matches;
-    private RelativeLayout lLayout;
+    private CoordinatorLayout lLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_match);
 
-        lLayout = (RelativeLayout) findViewById(R.id.linear_layout_bottom_sheet);
+        lLayout = (CoordinatorLayout) findViewById(R.id.activity_mainmatch);
+
+        //follow
+        //save matches from json file into array
+       // mMatchView = (TextView) findViewById(R.id.factTextView);
+        initViews();
+        initListeners();
 
         mMatcher = new RetrieveMatches();
         mMatcher.execute((Void) null);
@@ -111,4 +141,109 @@ public class MainMatchActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
+
+    ///-------------------------- j test
+
+    /**
+     * method to initialize the views
+     */
+    private void initViews() {
+
+
+        bottomSheetBehavior = BottomSheetBehavior.from(findViewById(R.id.bottomSheetLayout));
+        bottomSheetHeading = (TextView) findViewById(R.id.bottomSheetHeading);
+        expandBottomSheetButton = (Button) findViewById(R.id.expand_bottom_sheet_button);
+        collapseBottomSheetButton = (Button) findViewById(R.id.collapse_bottom_sheet_button);
+        hideBottomSheetButton = (Button) findViewById(R.id.hide_bottom_sheet_button);
+        showBottomSheetDialogButton = (Button) findViewById(R.id.show_bottom_sheet_dialog_button);
+
+
+    }
+
+
+    /**
+     * method to initialize the listeners
+     */
+    private void initListeners() {
+        // register the listener for button click
+        expandBottomSheetButton.setOnClickListener(this);
+        collapseBottomSheetButton.setOnClickListener(this);
+        hideBottomSheetButton.setOnClickListener(this);
+        showBottomSheetDialogButton.setOnClickListener(this);
+
+        // Capturing the callbacks for bottom sheet
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(View bottomSheet, int newState) {
+
+                if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+                    bottomSheetHeading.setText(getString(R.string.text_collapse_me));
+                } else {
+                    bottomSheetHeading.setText(getString(R.string.text_expand_me));
+                }
+
+                // Check Logs to see how bottom sheets behaves
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_COLLAPSED:
+                        Log.e("Bottom Sheet Behaviour", "STATE_COLLAPSED");
+                        break;
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        Log.e("Bottom Sheet Behaviour", "STATE_DRAGGING");
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED:
+                        Log.e("Bottom Sheet Behaviour", "STATE_EXPANDED");
+                        break;
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        Log.e("Bottom Sheet Behaviour", "STATE_HIDDEN");
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        Log.e("Bottom Sheet Behaviour", "STATE_SETTLING");
+                        break;
+                }
+            }
+
+
+            @Override
+            public void onSlide(View bottomSheet, float slideOffset) {
+
+            }
+        });
+
+
+    }
+
+    /**
+     * onClick Listener to capture button click
+     *
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.collapse_bottom_sheet_button:
+                // Collapsing the bottom sheet
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                break;
+            case R.id.expand_bottom_sheet_button:
+                // Expanding the bottom sheet
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                break;
+            case R.id.hide_bottom_sheet_button:
+                // Hiding the bottom sheet
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+                break;
+            case R.id.show_bottom_sheet_dialog_button:
+
+                break;
+
+        }
+    }
+
+
+
+
+
+
+    ///-------------------------- j test
 }
